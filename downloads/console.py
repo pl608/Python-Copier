12ed9dc303bec54fc2df6c3d1586758d57352f58
pl608 <78400949+pl608@users.noreply.github.com>
2022-01-14 11:46:09 -0800

    STILL doesnt work
    just thought i'd update it though because i need a little bit of help...

--- downloads/console.py
+++ downloads/console.py
@@ -1,1 +1,24 @@
-print("imported")
+import os, sys
+
+path = sys.argv[1]
+
+dirs = []
+files = []
+
+os.chdir(path)
+def dirsearch(dir: str):
+    global dirs
+    for x in os.listdir(dir):
+        dirs.append(x)
+        dirsearch(os.path.join(path,x))
+print(os.listdir(path))
+for x in os.listdir(path):
+    if os.path.isdir(x):
+        dirs.append(x)
+        dirsearch(x)
+    elif os.path.isfile(x) == True:
+        files.append(x)
+
+print(files)
+print("#####################")
+print(dirs)
